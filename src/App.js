6080b81c22aa94383599373d25fc71d31e6ff5c1
email <abe.solis2015@gmail.com>
2022-01-23 12:31:53 -0800

    adding navbar components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './components/Navbar'
 
+
 function App () {
   return (
     <>
-      <Navbar />
-      <h1 className='text-3xl font-bold underline'>Hello world </h1>
+    <Navbar />
+    <h1>Hey!</h1>
+      
     </>
   )
 }
