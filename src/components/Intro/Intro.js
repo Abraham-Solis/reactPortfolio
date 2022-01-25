e2784f254b8490532f83ff83493b3f7a39d35cf5
email <abe.solis2015@gmail.com>
2022-01-24 19:32:36 -0800

    Installed Gsaps and made an intro/nav

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useEffect } from 'react';
 import "./Intro.css";
+import { gsap } from "gsap";
 
 const Intro = () => {
+
+  // Using gsap for animation
+  const tl = gsap.timeline({defaults: {ease: 'power1.out'}})
+
+  //first needs class and then movement of object "%" and duration of animation
+  useEffect(() =>{
+
+    
+    tl.to('.text', {y:'0%', duration: 1.5, stagger: 0.25});
+    tl.to('.slider', {y:'-100%', duration: 1.5, delay: 0.5});
+    // Here start one second faster
+    tl.to('.intro', {y:'-100%', duration: 1}, '-=1');
+    //Specify a starting and ending point in this case the opacity
+    tl.fromTo('nav', {opacity:0}, {opacity:1, duration:1})
+    tl.fromTo('.big-text', { opacity: 0 }, { opacity: 1, duration: 1}, '-=1')
+  })
+
+
   return (
     <>
       <main>
@@ -20,15 +40,19 @@ const Intro = () => {
       <div className="intro">
         <div className="intro-text">
           <h1 className="hide">
-            <span className='text'>Creating Inovation</span>
+            <span className='text'>Creating Innovations </span>
           </h1>
         </div>
-        <h1 className="hide">
-          <span className='text'>For Everyday</span>
-        </h1>
-        <h1 className="hide">
-          <span className='text'>people.</span>
-        </h1>
+        <div className="intro-text">
+          <h1 className="hide">
+            <span className='text'> Made for </span>
+          </h1>
+        </div>
+        <div className="intro-text">
+          <h1 className="hide">
+            <span className='text'> Everyday people.</span>
+          </h1>
+        </div>
       </div>
       <div className="slider"></div>
     </>
