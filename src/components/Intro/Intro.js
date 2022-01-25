bf3125f46d8bf96e179ebeae43df554f33778be7
email <abe.solis2015@gmail.com>
2022-01-24 18:31:48 -0800

    Creating Landing Page and Nav

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,34 +1,38 @@
 import React from 'react';
+import "./Intro.css";
 
 const Intro = () => {
   return (
-      <>
-    <main>
-      <section className="landing">
-        <h1 id ="logo">Abraham Solis</h1>
-        <ul className="nav-links">
-          <li>About</li>
-          <li>Projects</li>
-          <li>Contact</li>
-        </ul>
-        <h2 className='big-text'>Weclome to my Portfolio</h2>
-      </section>
-    </main>
-    <div className="intro">
-      <div className="intro-text">
+    <>
+      <main>
+        <section className="landing">
+          <nav>
+            <h1 id="logo">Abraham Solis</h1>
+
+            <ul className="nav-links">
+              <li>About</li>
+              <li>Projects</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+          <h2 className='big-text'>Weclome to my Portfolio</h2>
+        </section>
+      </main>
+      <div className="intro">
+        <div className="intro-text">
+          <h1 className="hide">
+            <span className='text'>Creating Inovation</span>
+          </h1>
+        </div>
         <h1 className="hide">
-          <span className = 'text'>Creating Inovation</span>
+          <span className='text'>For Everyday</span>
+        </h1>
+        <h1 className="hide">
+          <span className='text'>people.</span>
         </h1>
       </div>
-        <h1 className="hide">
-          <span className = 'text'>For Everyday</span>
-        </h1>
-          <h1 className="hide">
-          <span className = 'text'>people.</span>
-        </h1>
-    </div>
-    <div className="slider"></div>
-  </>
+      <div className="slider"></div>
+    </>
   );
 };
 
