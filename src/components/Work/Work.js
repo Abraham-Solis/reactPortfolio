cb76e86be1270994ab3fb6a352592bc5e1dbf1e5
email <abe.solis2015@gmail.com>
2022-01-25 19:06:58 -0800

    Added Cards and Animations for Project Cards

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -7,15 +7,19 @@ import { Card, CardBody, CardTitle, CardSubtitle, CardText, CardLink } from "rea
 const Work = () => {
 
   useEffect(() => {
-    Aos.init({ duration: 3000})
+    Aos.init({ duration: 3000 })
   }, [])
 
   return (
     <body className='bg'>
-      <h2 className='header'>Projects:</h2>
-      <div className='row black'>
+
+      <h6 className='header'>Projects:</h6>
+
+      <div className='row'>
         <div className='col-1'></div>
-        <Card data-aos="fade-in"className='col-6'>
+        <Card
+          data-aos="fade-up"
+          className='col-6'>
           <CardBody>
             <CardTitle tag="h5">
               Card title
@@ -45,8 +49,91 @@ const Work = () => {
           </CardBody>
         </Card>
         <div className='col-4'></div>
-
       </div>
+
+      <br />
+      <br />
+      <br />
+
+      {/* Card 2  */}
+      <div className='row'>
+        <div className='col-4'></div>
+        <Card
+          data-aos="fade-left"
+          className='col-6'>
+          <CardBody>
+            <CardTitle tag="h5">
+              Card title
+            </CardTitle>
+            <CardSubtitle
+              className="mb-2 text-muted"
+              tag="h6"
+            >
+              Card subtitle
+            </CardSubtitle>
+          </CardBody>
+          <img
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            width="100%"
+          />
+          <CardBody>
+            <CardText>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </CardText>
+            <CardLink href="#">
+              Card Link
+            </CardLink>
+            <CardLink href="#">
+              Another Link
+            </CardLink>
+          </CardBody>
+        </Card>
+        <div className='col-1'></div>
+      </div>
+
+      <br />
+      <br />
+      <br />
+
+{/* Card 3 */}
+
+      <div className='row'>
+        <div className='col-1'></div>
+        <Card
+          data-aos="fade-right"
+          className='col-6'>
+          <CardBody>
+            <CardTitle tag="h5">
+              Card title
+            </CardTitle>
+            <CardSubtitle
+              className="mb-2 text-muted"
+              tag="h6"
+            >
+              Card subtitle
+            </CardSubtitle>
+          </CardBody>
+          <img
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            width="100%"
+          />
+          <CardBody>
+            <CardText>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </CardText>
+            <CardLink href="#">
+              Card Link
+            </CardLink>
+            <CardLink href="#">
+              Another Link
+            </CardLink>
+          </CardBody>
+        </Card>
+        <div className='col-4'></div>
+      </div>
+
     </body >
   );
 };
